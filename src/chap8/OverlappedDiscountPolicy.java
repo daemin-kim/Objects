7a4e40776aa442866837a86ffc13bf0260241000
daemin-kim <kdm0068@gmail.com>
2023-08-04 17:35:22 +0900

    add chap8

---
+++ src/chap8/OverlappedDiscountPolicy.java
@@ -0,0 +1,32 @@
+package chap8;
+
+import chap2.initial.DiscountPolicy;
+import chap2.initial.Money;
+import chap2.initial.Screening;
+import chap6.Theather.DiscountCondition;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OverlappedDiscountPolicy implements DiscountPolicy {
+
+    private List<DiscountPolicy> discountPolicies = new ArrayList<>();
+
+    public OverlappedDiscountPolicy(DiscountPolicy ... discountPolicies) {
+        this.discountPolicies = Arrays.asList(discountPolicies);
+    }
+
+    protected Money getDiscountAmount(Screening screening) {
+        Money result = Money.ZERO;
+        for (DiscountPolicy each : discountPolicies) {
+            result = result.plus(each.calculateDiscountAmount(screening));
+        }
+        return result;
+    }
+
+    @Override
+    public Money calculateDiscountAmount(Screening screening) {
+        return null;
+    }
+}
